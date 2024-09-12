ab3df191ef8a7b114bd12807224912b974882779
Ayush tomar <ayushtomar388@gmail.com>
2024-09-12 23:14:14 +0530

    Page Created

---
+++ src/app/javascript/page.tsx
@@ -0,0 +1,10 @@
+const JavascriptPage = () => {
+    return ( 
+        <div>
+            <a href="javascript/resources">Resources</a>
+
+        </div>
+     );
+}
+ 
+export default JavascriptPage;
