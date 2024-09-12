3a656ceeb136a6a397d20e52700eccb18119b40e
Ayush tomar <ayushtomar388@gmail.com>
2024-09-12 23:58:32 +0530

    Github resources page created

---
+++ src/app/github/resources/page.tsx
@@ -0,0 +1,11 @@
+const ResourcesPage = () => {
+    return ( 
+        <div>
+            Github resources
+
+            <a href="https://youtu.be/apGV9Kg7ics?si=HmkywrCmGEsQKqS_" target="_blank">https://youtu.be/apGV9Kg7ics?si=HmkywrCmGEsQKqS_</a>
+        </div>
+     );
+}
+ 
+export default ResourcesPage;
